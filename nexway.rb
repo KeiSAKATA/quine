7d7c218c7fc8dc3ae266958bc9346fe700d98624
KeiSAKATA <kei_sakata@nexway.co.jp>
2024-05-27 09:56:02 +0900

    first commit

---
+++ nexway.rb
@@ -0,0 +1,45 @@
+                       eval$s=
+                          %w'b
+                        ="BAhsK                            wIvA           gAA       gD8AAAAAAAAAAAA     AAAA         AAAA    AAAA        AAA         AAA          AA8         AAAA          AAA
+                        AAAAAAAA                           AAAA           AAA       AAAAAAAAAAfwAAA     HgAHP        D/Bw    /g4Q        EO4        AAcw         ANwA          AAAA         P8A
+                        AAB4ABzw/                          wcf4O          EBD       vAAHoAHcAAAAAD/      AQAA       +AAc     8P8H        HvDg       AR7w         AD6AB         3gAA        AAg/
+                     w  MAAPgBHPD/                         BzzwwA         Ef8       AA+AA84AAAAMP8H       AAD4      ARxw      AAA       8eMAD       H3AA         PwAPP          AAAA       Dj/
+                    Dw  AA+AMccAAAe                        DjAAx9         4AH       8AD                   hwAA     AA8/       x8AA      PgDHH       AAA         Hg8wAM          /eIB      3AB4
+                   eAA  AAPv8/AAD4Bx                       xwAADwH        oCD       P3i                    AdwA    cHg        AAAD      //fwA      A+A8         ccAAA8B          6Ah      zs4
+                  gPMA  PA8AAIA///8AA                      HgPHHAA        AOA       PgI                    c7OM   DjAD        gPAA      DAP///     AQB4        Hhxw AAD          gD4C    Hezz
+                 A4wD4  BwAA4D///wMAeB                     4c8P8AwA       cAx       3M8                     wOEB 8AcA          APA     ///8HAH     g8HP        D/AI AHA           Mdx    HODh
+                AfADAA  D4P///DwB4fBzw/                    wDABwDP8R      zgw       QHg                     AwAA /D8A          AAAA    eHg ccA     AA4         A8A  7/Ee          4MAD  4AEA
+               AP4/AAA  AAHjwHHAAAOAPAO7                   gHvD AA+A      BAA       D/P                      wAAAAB4           8B1w    AAD wHg     Du4        A7w/   wPg           AQA  A/z/
+              /P/4AeOA  dcAAA8BwA7uAP8P8H                  4AEA  gP8/     /z/       +AH                      jAH3AAA           Hg8A    P7A D/j/   B+AB        AID/   P/8           //gF4wB9
+             wAAB4eAB8  wA/4/wfgAQCA/z//P/                 4BeI  AfcA     AAP       HgAfMAPOAAP4              AEAgP             8//   z/+A  HiA   H3AA        AD7    wAHz           ABzwAD+
+            ABAID/P/8/  /gB4AB/w/wce8AB8gAc                8AB7   gAQA    A/z       //P34AeAAe8P               8HD+             ABP   IAH   HAA   e4A        EAAP    4//z           8+AHgA
+           HvD/Bw/gAzi  ABx4AHuABAAD8P/8/HgA               AAAA   AAAAA   AAA       AAAAAAAAAAAA              A+D//             Pw4A  AAA   AAAA  AAA        AAAA     AAA            AAAAA
+          AAAPA//z8GAA                                     AAAA    AAAA   AAA       AAA                      AAAAAAA            AAAD gP/8   /AgA AAAA        AAA      AAAA           AAAA
+         AAAAAAAAAAwD/                                     /PwA     AAAA  AAA       AAA                      AAAAAAA             AAA AAA     AAA IA//       z8AA      AAAA           AAAA
+        AAAAAAAAAAAAAA                                     AAAA     AP/8/ AAA       AAA                     AAAA AAAA            AAA AAA     AAA AAA        AAAD7/PwAAAAAA           AAAA
+        AAAAAAAAAAAAAA  AAAAA8/x8AAPjN   AxP7+Tz           gAQA      AAAA AAA       AAA                     AAAO  P8P            AAA gTA     AzQwgmM        AMAAAAAAAAAAAAA          ADD/
+       BwAAIEwAM0MIAhB  wTvQAAAAAAAAAA   AAg/wMA           ACDM       AVNDeAI       QUF                    u0AQ   AAAA           AAAAAAA      P8BAAA       gDAPTQ3gCkBNRNAE          AAAA
+       AAAAAAAD/AAAAIA  wCk0MIAhASUTQB   AAAAAAAA          AAAA       fwAAACB       MAp                    NDCC    YwE9           k2AQA       AAAAAA       AAAAD4AAAAgzAMTQ          /g84
+       BGO9wEAAAAAAAAA  AAAAAAAAAAAAAA   AAAAAAAD          AAAA        AAAAAA       AAA                   AAAA     AAAA           AAAAA       AAAAAA       Aw"          ;n=M         arsh
+       al.load(b.unpac  k("m")[0]);e="   eval$s=           %w"<        <39<<(       $s*                  3);o=      "";j          =-1;0       .upto       (45*          200-         1){|
+       i|;o<<((n[i]==1  )?e[j+=1]:32);   o<<((i%           200=         =(199       ))?10:"");};o[-      65,6       ]=""          <<39<        <".j       oin"           ;put        s(o)
+        #b="BAhsKwIvAg  AAgD8AAAAAAAAA   AAAAAA            AAAA          AAAA       AAAAAAAAA8AAAAA     AAAA         AAAA         AAAA         AAAA       AAA            AAAA        AAAA
+         fwAAAHgAHPD/B  w/g4QEO4AAcwAN   wAAAA             AP8A          AAB4       ABzw/wcf4OEBDvA     AHoA         HcAAA         AAD         /AQA      A+AA            c8P8        HHvD
+          gAR7wAD6AB3g  AAAAg/wMAAPgBH   PD/B
+           zzwwAEf8AA+  AA84AAAAMP8HAA   D4A
+            RxwAAA8eMA  DH3AAPwAPPAAAA   Dj
+             /DwAA+AMc  cAAAeDjAAx94AH   8
+              ADhwAAAA  8/x8AAPgDHHAAA
+               Hg8wAM/  eIB3AB4eAAAAPv
+                8/AAD4  BxxwAADwHoCDP3
+                 iAdwA  cHgAAAD//fwAA+
+                  A8cc  AAA8B6Ahzs4gP                      MAPA8A AI  A///      8A  A   Hg PHHAAA  OAPgI  c7OM       DjAD
+                   gPA  ADAP///AQB4H                         h    xw  A         AD  gD  4C    H    e     zz  A      4w  D4
+                    Bw  AA4D///wMAe                          B    4c  8         P8  Aw  Ac    A    x     3          M       8wO  EB8  A   c AAPA
+                     /  //8HAHg8HP                           D    /A  IAH       AM  d x HO    D    hAfA  D          A       A D 4P // /   D wB 4f
+                        Bzw/wDABw                            D    P8    Rz      gw  Q HgAw    A    A/D8  A          A  AAA  e   H   g c   c AA  A
+                        4A8A7/Ee                             4    MA     D      4A  E  AAP    4    /     A          A    A  A   A   H j   w HH  A
+                        AAOAPAO                              7    gH  v  D      AA  +  ABA    A    D     /P  w      AA  AA  B   4  8B 1w AA Dw  H
+                         gDu4A                               7    w/  wPgA      QA  A   /z    /    /P/4A  eOAd       cAAA   8    BwA   7uAP 8P8H4
+                                                                                                                                            AE
+                                                                                                                                        '.join
